facc39c9e5df086ca32a1f81692b4ba38eaac435
Yuriy Kharchenko <rm1945@yandex.ru>
2020-07-09 18:10:46 +0300

    Implement create_task and nearby_tasks

--- apps/db/lib/db.ex
+++ apps/db/lib/db.ex
@@ -1,18 +1,42 @@
 defmodule Db do
+  import Ecto.Query
+  alias Db.{Repo, Token, Task, User}
+
   @moduledoc """
-  Documentation for Db.
+  Public API of the database.
   """
 
-  @doc """
-  Hello world.
+  @type geo_location :: {float, float}
 
-  ## Examples
+  @spec create_task(geo_location, geo_location, String.t()) :: {:ok, %Task{}}
+  def create_task({lat1, long1}, {lat2, long2}, description \\ "") do
+    %Task{
+      status: Task.status_new,
+      description: description,
+      lat1: lat1,
+      long1: long1,
+      lat2: lat2,
+      long2: long2
+    }
+    |> Repo.insert
+  end
 
-      iex> Db.hello()
-      :world
+  @spec nearby_tasks(geo_location) :: any()
+  def nearby_tasks({lat, long}) do
+    query =
+      from t in Task,
+        where: t.status == ^Task.status_new,
+        select: %{
+          id: t.id,
+          description: t.description,
+          distance: fragment("(POINT(?, ?) <@> POINT(?, ?)) AS distance", t.long1, t.lat1, ^long, ^lat)
+        },
+        order_by: [asc: fragment("distance")]
 
-  """
-  def hello do
-    :world
+    query |> Repo.all
+  end
+
+  defp generate_token do
+    :crypto.strong_rand_bytes(32) |> Base.url_encode64
   end
 end
--- apps/db/test/db_test.exs
+++ apps/db/test/db_test.exs
@@ -1,8 +1,32 @@
 defmodule DbTest do
-  use ExUnit.Case
-  doctest Db
+  use Db.RepoCase
 
-  test "greets the world" do
-    assert Db.hello() == :world
+  test ".create_task" do
+    {:ok, task} = Db.create_task({0.1, 0.2}, {0.3, 0.4}, "test")
+
+    assert task.lat1 == 0.1
+    assert task.long1 == 0.2
+    assert task.lat2 == 0.3
+    assert task.long2 == 0.4
+    assert task.description == "test"
+  end
+
+  test ".nearby_tasks" do
+    milan = {45.463619, 9.188120}
+    genova = {44.4056, 8.9463}
+    bologna = {44.4949, 11.3426}
+    florence = {43.7696, 11.2558}
+    rome = {41.9028, 12.4964}
+
+    {:ok, _} = Db.create_task(genova, bologna, "Genova -> Bologna")
+    {:ok, _} = Db.create_task(bologna, genova, "Bologna -> Genova")
+    {:ok, _} = Db.create_task(florence, rome, "Florence -> Rome")
+
+    get_result = fn loc ->
+      Db.nearby_tasks(loc) |> Enum.map(fn rec -> rec.description end)
+    end
+
+    assert ["Genova -> Bologna", "Bologna -> Genova", "Florence -> Rome"] = get_result.(milan)
+    assert ["Bologna -> Genova", "Florence -> Rome", "Genova -> Bologna"] = get_result.(bologna)
   end
 end
